74348b1e55037c9b2207f8c733dd54eee4ab8ccf
AlbinOdelstav <albin.odelstav@outlook.com>
2022-05-09 21:28:17 +0200

    a

--- app/src/main/java/com/cringe/quitaddiction/MainActivity.kt
+++ app/src/main/java/com/cringe/quitaddiction/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.cringe.quitaddiction.ui.theme.EditText
 import com.cringe.quitaddiction.ui.theme.QuitAddictionTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    EditText(textba = "Hello $name!")
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/cringe/quitaddiction/ui/theme/input.kt
@@ -0,0 +1,31 @@
+package com.cringe.quitaddiction.ui.theme
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import java.lang.reflect.Modifier
+
+@Composable
+fun EditText(textba: String) {
+    var text by remember { mutableStateOf(textba) }
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+
+    }
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    QuitAddictionTheme {
+        EditText(textba = "hello")
+    }
+}
