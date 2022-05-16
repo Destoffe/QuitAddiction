266495f1dbfd2d0a864abed3376e5ae4a17d0f42
AlbinOdelstav <albin.odelstav@outlook.com>
2022-05-16 21:39:47 +0200

    welcome view

--- app/src/main/java/com/cringe/quitaddiction/MainActivity.kt
+++ app/src/main/java/com/cringe/quitaddiction/MainActivity.kt
@@ -3,13 +3,23 @@ package com.cringe.quitaddiction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cringe.quitaddiction.ui.theme.ExposedEditText
 import com.cringe.quitaddiction.ui.theme.MoneySavedText
+import com.cringe.quitaddiction.ui.theme.OutlinedEditText
 import com.cringe.quitaddiction.ui.theme.QuitAddictionTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,10 +30,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QuitAddictionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                WelcomeView()
+            }
+        }
+    }
+}
+
+@Composable
+fun WelcomeView() {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 64.dp),
+                text = "Welcome!",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center
+            )
+            ExposedEditText("Android")
+            OutlinedEditText("How much do you spend daily?")
+            OutlinedEditText("What's your first goal?")
+        }
+
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+                onClick = {}) {
+                Text("LETS QUIT")
             }
         }
     }
@@ -34,10 +73,11 @@ fun Dashboard(name: String) {
     MoneySavedText("5")
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     QuitAddictionTheme {
-        Dashboard("Android")
+        WelcomeView()
     }
 }
--- app/src/main/java/com/cringe/quitaddiction/ui/theme/input.kt
+++ app/src/main/java/com/cringe/quitaddiction/ui/theme/input.kt
@@ -1,31 +1,73 @@
 package com.cringe.quitaddiction.ui.theme
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun EditText(textba: String) {
-    var text by remember { mutableStateOf(textba) }
+fun OutlinedEditText(label: String) {
+    var text by remember { mutableStateOf("") }
 
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
 
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(label) }
+        )
     }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Label") }
-    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ExposedEditText(inputText: String) {
+    var expanded by remember { mutableStateOf(true) }
+    var options = listOf("alkohol", "skitsamma", "test")
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+    Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }) {
+            TextField(
+                value = selectedOptionText,
+                onValueChange = {},
+                label = { Text("What's your addiction?") },
+                readOnly = true
+            )
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                options.forEach {
+                    DropdownMenuItem(onClick = {
+                        expanded = false
+                        selectedOptionText = it
+                    }) {
+                        Text(text = it)
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
     QuitAddictionTheme {
-        EditText(textba = "hello")
+        ExposedEditText(inputText = "hello")
     }
 }
